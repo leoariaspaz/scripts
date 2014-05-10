207bbaa6ef2d15579917602a77a8fdef9db06363
Leonardo Arias Paz <leoariaspaz@gmail.com>
2014-05-10 19:40:48 -0300

    Elimina enlaces innecesarios y filtra resultados de un número de teléfono

--- download_and_merge.rb
+++ download_and_merge.rb
@@ -1,11 +1,16 @@
+#!/usr/bin/ruby
 require 'open-uri'
 
 dir = File.expand_path(File.dirname(__FILE__))
-f_todos = File.new("#{dir}/todos.html", "w")
+f_todos = File.new("#{dir}/todos.html", "w+")
+HEADER = %Q{<!doctype html><html><head><meta charset="utf-8"><title>Untitled</title></head><body>}
+FOOTER = %Q{</body></html>}
+f_todos.write(HEADER)
 1.upto(7) do |i|
 	print "Procesando página #{i} "
 	html = open("http://www.clasificadospanorama.com/clasif.main.php?op=subrubrores&ID=29&tr=30&page=#{i}").read
-	f = File.new("#{dir}/#{i}.html", "w")
+	f = File.new("#{dir}/#{i}.html", "w+")
+	f.write(HEADER)
 	ult_url = ""
 	html.scan(/getAviso_m\(([^)]+)\)/).uniq.map do |r|
 		print '.'
@@ -15,13 +20,20 @@ f_todos = File.new("#{dir}/todos.html", "w")
 					"&tr=#{params[4]}&claves=&origen="
 		ult_url = url
 		aviso = open(url).read
-		aviso = aviso.gsub(/(<br \/>\n){2,}/, "")
-		f.write "<hr/>"
-		f.write(aviso)
-		f_todos.write "<hr/>"
-		f_todos.write(aviso)
+		if (aviso =~ /4216271/).nil?
+			aviso.gsub!(/(<br \/>\n){2,}/, "")
+			aviso.gsub!(%r#<table width="98%".*?</table>#m, "")
+			aviso.gsub!(/<a.+onclick='filtrarBusqueda.+/i, "")
+			aviso.gsub!(%r#<div id="CuadroAmpliada".*?</div>#im, "")
+			f.write "<hr/>"
+			f.write(aviso)
+			f_todos.write "<hr/>"
+			f_todos.write(aviso)
+		end
 	end
 	print "\n"
+	f.write(FOOTER)
 	f.close
 end
+f_todos.write(FOOTER)
 f_todos.close
